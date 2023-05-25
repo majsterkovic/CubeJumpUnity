fb0498fe16a28a84431f4b0982706858c969d0cc
majsterkovic <mariusz8h@o2.pl>
2023-05-25 21:27:00 +0200

    Upload project

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject obstacle;
+    public Transform spawnPoint;
+    int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObstacles()
+    {
+        while(true)
+        {
+            float waitTime = Random.Range(0.5f, 2f); //do testów 0.2f, 02
+
+            yield return new WaitForSeconds(waitTime);
+
+            Instantiate(obstacle, spawnPoint.position, Quaternion.identity);
+        }
+    }
+
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+
+    }
+
+    public void GameStart()
+    {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
+        StartCoroutine("SpawnObstacles");
+        InvokeRepeating("ScoreUp", 2f, 1f);
+    }
+
+}
